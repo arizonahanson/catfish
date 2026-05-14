9b408437b1ffe6056c0bcdbb5b0a751371d2b4cc
Arizona Hanson <arizona@starlig.ht>
2026-05-14 12:50:21 -0400

    tweak(fish): mode prompt in ASCII

--- fish/dot-config/fish/functions/fish_mode_prompt.fish
+++ fish/dot-config/fish/functions/fish_mode_prompt.fish
@@ -5,19 +5,19 @@ function fish_mode_prompt --description 'Display vi prompt mode'
         switch $fish_bind_mode
             case default
                 set_color --bold $ct_lavender
-                echo '●'
+                echo ':'
             case insert
                 set_color --bold $ct_green
-                echo '❱'
+                echo '%'
             case replace_one
                 set_color --bold $ct_sapphire
-                echo '🟂'
+                echo '*'
             case replace
                 set_color --bold $ct_teal
-                echo '🟋'
+                echo '*'
             case visual
                 set_color --bold $ct_blue
-                echo '🟆'
+                echo '@'
         end
         set_color normal
 		echo -n ' '
